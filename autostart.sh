7646b428f6bb1a9dc7605e3b15f980dea567a151
MM-2103 <mohsen.menem1@gmail.com>
2023-08-12 23:15:01 +0200

    first commit

---
+++ autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xrandr --output HDMI-0 --pos 0x0 --primary --auto --output eDP-1-1 --pos 2560x180 --auto
+sleep 1 && \
+nitrogen --restore
+volctl
+picom --config ~/.config/picom/picom.conf
