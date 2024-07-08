33748b0210fb51bcc3bd90dc1f8b7327ff0406d0
MM-2103 <mohsen.menem1@gmail.com>
2024-07-08 20:19:49 +0200

    couple changes

--- autostart.sh
+++ autostart.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 autorandr -c
 xfce4-clipman &
-thunderbird &
 nm-applet &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 kdeconnect-indicator &
 xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
 xfce4-power-manager &
-ckb-next -b &
-cbatticon -n &
-feh --bg-scale ~/Pictures/Wallpaper/berserk-forest.jpg 
+feh --bg-scale ~/Pictures/Wallpaper/berserk-forest.png 
 picom --config ~/.config/picom/picom.conf &
